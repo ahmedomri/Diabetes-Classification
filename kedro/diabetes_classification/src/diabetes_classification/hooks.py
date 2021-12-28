d4301dbb4c822afe1f2df9feb915e2ca52313d67
ahmedomri <omri6346@gmail.com>
2021-12-28 22:40:14 +0100

    add first packages && configuration of hooks

--- kedro/diabetes_classification/src/diabetes_classification/hooks.py
+++ kedro/diabetes_classification/src/diabetes_classification/hooks.py
@@ -5,23 +5,43 @@ from kedro.config import ConfigLoader
 from kedro.framework.hooks import hook_impl
 from kedro.io import DataCatalog
 from kedro.versioning import Journal
+from kedro.pipeline import Pipeline
+from .pipelines import data_engineering as de
+from .pipelines import data_science as ds
 
 
 class ProjectHooks:
     @hook_impl
+    def register_pipelines(self) -> Dict[str, Pipeline]:
+        """Register the project's pipeline.
+
+        Returns:
+            A mapping from a pipeline name to a ``Pipeline`` object.
+
+        """
+        data_engineering_pipeline = de.create_pipeline()
+        data_science_pipeline = ds.create_pipeline()
+
+        return {
+            "de": data_engineering_pipeline,
+            "ds": data_science_pipeline,
+            "__default__": data_engineering_pipeline+data_science_pipeline,
+        }
+
+    @hook_impl
     def register_config_loader(
-        self, conf_paths: Iterable[str], env: str, extra_params: Dict[str, Any],
+            self, conf_paths: Iterable[str], env: str, extra_params: Dict[str, Any],
     ) -> ConfigLoader:
         return ConfigLoader(conf_paths)
 
     @hook_impl
     def register_catalog(
-        self,
-        catalog: Optional[Dict[str, Dict[str, Any]]],
-        credentials: Dict[str, Dict[str, Any]],
-        load_versions: Dict[str, str],
-        save_version: str,
-        journal: Journal,
+            self,
+            catalog: Optional[Dict[str, Dict[str, Any]]],
+            credentials: Dict[str, Dict[str, Any]],
+            load_versions: Dict[str, str],
+            save_version: str,
+            journal: Journal,
     ) -> DataCatalog:
         return DataCatalog.from_config(
             catalog, credentials, load_versions, save_version, journal
---
+++ kedro/diabetes_classification/src/diabetes_classification/pipelines/data_engineering/pipeline.py
@@ -0,0 +1,9 @@
+from kedro.pipeline import Pipeline, node
+
+
+def create_pipeline(**kwargs):
+    return Pipeline(
+        [
+            # Write your code
+        ]
+    )
