a73762faaefcc63625f29f5a97f7252edb56c315
AbdelhayAboulbaraket <abdelhay.aboulbaraket@gmail.com>
2022-01-24 17:41:27 +0100

    last commit

--- kedro/diabetes_classification/src/diabetes_classification/pipelines/data_engineering/nodes/replace_null_values.py
+++ kedro/diabetes_classification/src/diabetes_classification/pipelines/data_engineering/nodes/replace_null_values.py
@@ -5,8 +5,10 @@ logger = logging.getLogger(__name__)
 
 
 def replace_null_values(diabetes_data: pd.DataFrame) -> pd.DataFrame:
+    # Distribution Symétrique -> mean
     diabetes_data['Glucose'].fillna(diabetes_data['Glucose'].mean(), inplace=True)
     diabetes_data['BloodPressure'].fillna(diabetes_data['BloodPressure'].mean(), inplace=True)
+    # Distribution Asymétrique -> median
     diabetes_data['SkinThickness'].fillna(diabetes_data['SkinThickness'].median(), inplace=True)
     diabetes_data['Insulin'].fillna(diabetes_data['Insulin'].median(), inplace=True)
     diabetes_data['BMI'].fillna(diabetes_data['BMI'].median(), inplace=True)
